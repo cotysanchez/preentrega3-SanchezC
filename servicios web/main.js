8981fd696bb015a430df9adcc9450d2e67fb8406
Constanza Sanchez <constanzasanchezdigital@gmail.com>
2023-08-30 20:15:57 -0300

    modificando js

--- servicios web/main.js
+++ servicios web/main.js
@@ -1,4 +1,4 @@
-
+const cursos = document.getElementById('cursos');
 //Creamos el Array de Objetos para los Servicios
 const servicios = [
   { id: 1, nombre: 'Desarrollo Web', precio: 580 },
@@ -9,28 +9,36 @@ const servicios = [
 //guardamos el arreglo en el stoage y le damos formato JSON
 localStorage.setItem('servicios', JSON.stringify(servicios));
 //obtenemos el arreglo de servicios del Storage
-let carrito= JSON.parse(localStorage.getItem("servicios")) || [];
+let carrito = [];
 
 //guardamos el carrito en el Storage
-localStorage.setItem("carrito", JSON.stringify("carrito"));
+localStorage.setItem('carrito', JSON.stringify('carrito'));
 //obtenemos el carrito del Storage
 
 //funcion para agregar servicios al carrito
 const agregar = (id) => {
   let servicio = servicios.find((servicio) => servicio.id === id);
-  carrito.push(servicio);
-  localStorage.setItem('carrito', JSON.stringify('carrito'));
-  console.log("Servicio agregado al Carrito:", servicio);
-}
+  let seleccion = {
+    id: servicio.id,
+    nombre: servicio.nombre,
+    precio: servicio.precio,
+  };
+  carrito.push(seleccion);
+  console.log(seleccion);
+  localStorage.setItem('carrito', carrito);
+  // console.log("Servicio agregado al Carrito:", servicio);
+  mostrarCarrito();
+  calcularTotal();
+};
 
 //funcion para eliminar servicios del carrito
-const eliminarDelCarrito = (id) =>{
+const eliminarDelCarrito = (id) => {
   let index = carrito.findIndex((servicio) => servicio.id === id);
-  if (index !== -1){
+  if (index !== -1) {
     carrito.splice(index, 1);
-    localStorage.setItem("carrito",JSON.stringify("carrito"));
+    localStorage.setItem('carrito', JSON.stringify('carrito'));
   }
-}
+};
 
 //recorremos los servicios
 servicios.forEach((servicio) => {
@@ -41,60 +49,59 @@ servicios.forEach((servicio) => {
   <b>$${servicio.precio}</b>
   <button id="boton${servicio.id}">Agregar al Carrito</button>
   `;
+
+  let botonEliminar = document.createElement('button');
+  botonEliminar.addEventListener('click', () =>
+    eliminarDelCarrito(servicio.id)
+  );
+  console.log(eliminarDelCarrito);
+
   div.className = 'gris';
 
   //agregamos el docuemeto al body en un div
-  document.body.append(div);
+  cursos.append(div);
 
   //damos un evento  click al boton, pidiendole que agregue el id del servicio
   let boton = document.getElementById(`boton${servicio.id}`);
   boton.addEventListener('click', () => agregar(servicio.id));
-
-  
-/*
-  //EL BOTON ELIMINAR NO CUMPLE LA FUNCION :O
-  let botonEliminar = document.createElement('button');
-  botonEliminar.textContent = 'Eliminar del Carrito';
-  botonEliminar.addEventListener('click', () => eliminarDelCarrito(servicio.id));
-
-  div.appendChild(botonEliminar);
-  */
 });
 
+const contenedorCursos = document.getElementById('contenedorCursos');
+const precioTotal = document.getElementById('precioTotal');
 
-/*
- //funcion para mostrar el carrito con los servicios seleccionados
- const mostrarCarrito =() => {
-  let carritoContainer = document.createElement("div");
+//funcion para mostrar el carrito con los servicios seleccionados
+function mostrarCarrito() {
+  contenedorCursos.innerHTML = '';
+
+  let servicioInfo = document.createElement('div');
+  let carritoContainer = document.createElement('div');
+  carritoContainer.innerHTML = '';
+  servicioInfo.innerHTML = '';
   carritoContainer.innerHTML = `<h2> Carrito de Compras</h2>`;
 
-  let total = 0;
-
-  carrito.forEach((servicio) =>{
-    let servicioInfo = document.createElement('div');
-    servicioInfo.innerHTML = `
+  carrito.forEach((servicio) => {
+    servicioInfo.innerHTML += `
     <p>Nombre:${servicio.nombre}</p>
     <p>Precio:$${servicio.precio}</p>
+    <button id="id${servicio.id}">Eliminar</button>
     `;
 
     carritoContainer.appendChild(servicioInfo);
-
-    total += servicio.precio;
-
+    contenedorCursos.appendChild(carritoContainer);
   });
+}
 
-  let totalElement = document.createElement("div");
+function calcularTotal() {
+  precioTotal.innerHTML = '';
+  let total = 0;
+  carrito.forEach((prod) => {
+    total += prod.precio;
+  });
+  let totalElement = document.createElement('div');
   totalElement.innerHTML = ` <h3> Total: $${total}</h3>`;
 
-  carritoContainer.appendChild(totalElement);
+  precioTotal.appendChild(totalElement);
+}
 
-  document.body.appendChild(carritoContainer);
-
-
- };
-
- 
- mostrarCarrito();
-
-
-*/
+// mostrarCarrito();
+// calcularTotal()
