a77015466f49cc12d3f6ef9dfa72fa42af13bae8
Constanza Sanchez <constanzasanchezdigital@gmail.com>
2023-09-12 12:04:46 -0300

    agregando fetch

--- servicios web/main.js
+++ servicios web/main.js
@@ -34,16 +34,31 @@ let boton = '';
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    servicios.forEach((servicio) => {
+  'DOMContentLoaded',() => {
+    cargarDatos();
+    mostrarCarrito(carrito);
+    });
+
+// Crear función asincrónica para cargar los datos desde data.json
+async function cargarDatos() {
+  try {
+    const response = await fetch('./servicios web/data.json'); // Realizar una solicitud GET al archivo data.json
+    if (!response.ok) {
+      throw new Error('No se pudo cargar el archivo JSON.');
+    }
+
+    const data = await response.json(); // Convertir la respuesta en formato JSON
+
+    // Iterar sobre los datos y mostrar los servicios
+    data.servicios.forEach((servicio) => {
       mostrarServicios(servicio);
     });
 
-    mostrarCarrito(carrito);
-  },
-  false
-);
+  } catch (error) {
+    console.error('Se produjo un error:', error);
+  }
+}
+
 
 //creamos la funcion para mostrar los servicios
 function mostrarServicios(servicio) {
@@ -96,8 +111,6 @@ function agregar(e) {
   localStorage.setItem('carrito', JSON.stringify(carrito));
 
   mostrarCarrito(carrito);
-  
-  
 }
 
 //funcion para mostrar el carrito con los servicios seleccionados
@@ -132,7 +145,6 @@ function mostrarCarrito(carritoActualizado) {
 }
 
 
-
 //Funcion Eliminar Servicio del Carrito
 const eliminarDelCarrito = (e) => {
   if (e.target.classList.contains('eliminar-btn')) {
@@ -157,6 +169,7 @@ function calcularTotal(carrito) {
   carrito.forEach((servicio) => {
     total += servicio.precio * servicio.cantidad;
   });
+  //Carrito Vacío
   let totalElement = document.createElement('div');
   totalElement.innerHTML =
     total === 0
